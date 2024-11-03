bb2caa0490bc6f9dd9ea8b9e203421ceb1f5ffe7
Mateusz Rodz <50263944+Mate-usz@users.noreply.github.com>
2024-11-03 18:00:03 +0100

    add header/hero

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col mx-auto max-w-3xl px-8">
       <Navbar />
+      <Header />
     </div>
   );
 }
