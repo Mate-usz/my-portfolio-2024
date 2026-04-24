321d160138bdea9ddf9827266bca178c14acc036
Mateusz Rodz <rodzmateusz@gmail.com>
2026-04-24 20:14:46 +0200

    fix server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== "production";
 
 const hostname = "localhost";
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = next({ dev, hostname, port });
 
